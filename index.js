bb3006066aab71c38ac1c9ee00965cd1fd80c4f8
Luh C <ametiista@yahoo.com>
2024-05-19 19:14:30 -0300

    projeto final

---
+++ index.js
@@ -0,0 +1,26 @@
+function calcularNivel(vitorias, derrotas) {
+    let saldoVitorias = vitorias - derrotas;
+    let nivel;
+
+    if (vitorias < 10) {
+        nivel = "Ferro";
+    } else if (vitorias >= 10 && vitorias <= 20){
+        nivel = "Bronze";
+    } else if (vitorias > 20 && vitorias <= 50) {
+        nivel = "Prata";
+    } else if (vitorias > 50 && vitorias <= 80) {
+        nivel = "Ouro";
+    } else if (vitorias > 80 && vitorias <= 90) {
+        nivel = "Diamante";
+    } else if (vitorias > 90 && vitorias <= 100) {
+        nivel = "Lendário";
+    } else {
+        nivel = "Imortal";
+    }
+
+    return `O Herói tem de saldo ${saldoVitorias} vitórias e está no nível de ${nivel}`;
+}
+
+ //Exemplo de uso de função
+ let mensagem = calcularNivel(105,2);
+ console.log(mensagem);
